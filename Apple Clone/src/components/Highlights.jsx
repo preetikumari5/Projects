19633696439244562a5133910ea6563a4c85ade5
preetikumari5 <147963preetikumari@gmail.com>
2024-07-28 09:59:15 +0530

    apple clone

---
+++ Apple Clone/src/components/Highlights.jsx
@@ -0,0 +1,9 @@
+const Highlights = () => {
+    return (
+      <section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
+
+      </section>
+    )
+  }
+  
+  export default Highlights
